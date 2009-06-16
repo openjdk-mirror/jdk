be40c0140b687d605c9183ef3fdcfa339950d668
andrew <none@none>
2009-06-16 10:50:35 +0100

    100072: awt_p.h incorporates a chunk of the XRender header Summary: Use XRender header directly rather than copying chunks locally

--- src/solaris/native/sun/awt/awt_GraphicsEnv.c
+++ src/solaris/native/sun/awt/awt_GraphicsEnv.c
@@ -337,7 +337,7 @@ getAllConfigs (JNIEnv *env, int screen, AwtScreenDataPtr screenDataPtr) {
     char errmsg[128];
     int xinawareScreen;
     void* xrenderLibHandle = NULL;
-    XRenderFindVisualFormatFunc *XRenderFindVisualFormat = NULL;
+    XRenderFindVisualFormatFunc* xrenderFindVisualFormat = NULL;
     int major_opcode, first_event, first_error;
 
     if (usingXinerama) {
@@ -435,9 +435,9 @@ getAllConfigs (JNIEnv *env, int screen, AwtScreenDataPtr screenDataPtr) {
 #endif
 
         if (xrenderLibHandle != NULL) {
-            XRenderFindVisualFormat =
+            xrenderFindVisualFormat =
                 (XRenderFindVisualFormatFunc*)dlsym(xrenderLibHandle,
-                                                    "XRenderFindVisualFormat");
+						    "XRenderFindVisualFormat");
         }
     }
 
@@ -454,7 +454,7 @@ getAllConfigs (JNIEnv *env, int screen, AwtScreenDataPtr screenDataPtr) {
         graphicsConfigs [ind]->awt_depth = pVITrue [i].depth;
         memcpy (&graphicsConfigs [ind]->awt_visInfo, &pVITrue [i],
                 sizeof (XVisualInfo));
-       if (XRenderFindVisualFormat != NULL) {
+       if (xrenderFindVisualFormat != NULL) {
             XRenderPictFormat *format = XRenderFindVisualFormat (awt_display,
                     pVITrue [i].visual);
             if (format &&
