81212fd550518af1fac52dea53666cfb1ee655db
Hamish Morrison <hamish@lavabit.com>
2012-05-28 00:27:19 +0100

    HaikuClipboard and HaikuDataTransferer
    Clipboard needs some native implementation, DataTransferer needs some
    investigation vis-a-vis Tracker and file formats

---
+++ src/solaris/classes/sun/hawt/HaikuClipboard.java
@@ -0,0 +1,122 @@
+/*
+ * Copyright (c) 2012, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.  Oracle designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Oracle in the LICENSE file that accompanied this code.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+
+package sun.hawt;
+
+import java.awt.datatransfer.*;
+import java.io.IOException;
+import java.util.*;
+
+import sun.awt.datatransfer.*;
+
+public class HaikuClipboard extends SunClipboard {
+
+    private native String[] nativeGetClipboardFormats();
+    private native byte[] nativeGetClipboardData(String format)
+        throws IOException;
+    public native void setData(byte[] data, long format);
+
+    public HaikuClipboard(String name) {
+        super(name);
+    }
+
+    @Override
+    public long getID() {
+        return 0;
+    }
+
+    @Override
+    protected void clearNativeContext() {
+    }
+
+    @Override
+    protected void setContentsNative(Transferable contents) {
+
+        // Don't use delayed Clipboard rendering for the Transferable's data.
+        // If we did that, we would call Transferable.getTransferData on
+        // the Toolkit thread, which is a security hole.
+        //
+        // Get all of the target formats into which the Transferable can be
+        // translated. Then, for each format, translate the data and post
+        // it to the Clipboard.
+        DataTransferer transferer = DataTransferer.getInstance();
+        Map<Long, DataFlavor> formatMap = transferer.
+            getFormatsForTransferable(contents, flavorMap);
+
+        for (long format : formatMap.keySet()) {
+            DataFlavor flavor = formatMap.get(format);
+
+            try {
+                byte[] bytes = transferer.
+                    translateTransferable(contents, flavor, format);
+                setData(bytes, format);
+            } catch (IOException e) {
+                // Fix 4696186: don't print exception if data with
+                // javaJVMLocalObjectMimeType failed to serialize.
+                // May remove this if-check when 5078787 is fixed.
+                if (!(flavor.isMimeTypeEqual(
+                        DataFlavor.javaJVMLocalObjectMimeType) &&
+                        e instanceof java.io.NotSerializableException)) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    @Override
+    protected long[] getClipboardFormats() {
+        String[] nativeFormats = nativeGetClipboardFormats();
+
+        long[] temporary = new long[nativeFormats.length];
+        HaikuDataTransferer transferer =
+            (HaikuDataTransferer)DataTransferer.getInstance();
+        int length = 0;
+        for (String nativeFormat : nativeFormats)
+            temporary[length++] = transferer.getFormatForNativeAsLong(
+                nativeFormat);
+        
+        long[] formats = new long[length];
+        System.arraycopy(temporary, 0, formats, 0, length);
+        return formats;
+    }
+
+    @Override
+    protected byte[] getClipboardData(long format) throws IOException {
+        HaikuDataTransferer transferer = (HaikuDataTransferer)
+            DataTransferer.getInstance();
+        String nativeFormat = transferer.getNativeForFormat(format);
+        return nativeGetClipboardData(nativeFormat);
+    }
+
+    @Override
+    protected void registerClipboardViewerChecked() {
+        // todo
+    }
+
+    @Override
+    protected void unregisterClipboardViewerChecked() {
+        // todo
+    }
+}
---
+++ src/solaris/classes/sun/hawt/HaikuDataTransferer.java
@@ -0,0 +1,234 @@
+/*
+ * Copyright (c) 2012, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.  Oracle designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Oracle in the LICENSE file that accompanied this code.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+
+package sun.hawt;
+
+import java.awt.*;
+import java.awt.image.*;
+import sun.awt.image.ImageRepresentation;
+
+import java.io.*;
+import java.net.URL;
+import java.text.Normalizer;
+import java.text.Normalizer.Form;
+import java.util.*;
+
+import java.awt.datatransfer.*;
+import sun.awt.datatransfer.*;
+
+public class HaikuDataTransferer extends DataTransferer {
+
+    // This is how we generate our unique longs for natives...    
+    private static long index = 0;
+
+    private static final Map<Long, String> formatToNative;
+    private static final Map<String, Long> nativeToFormat;
+
+    private static HaikuDataTransferer transferer;
+    private ToolkitThreadBlockedHandler handler;
+
+    public final long FORMAT_INVALID  = addNative("");
+    public final long FORMAT_STRING   = addNative("text/plain");
+    public final long FORMAT_URI_LIST = addNative("text/uri-list");
+    public final long FORMAT_JPEG     = addNative("image/jpeg");
+    public final long FORMAT_PNG      = addNative("image/png");
+    public final long FORMAT_TIFF     = addNative("image/tiff");
+
+    static {
+        Map<Long, String> formatMap = new HashMap<>();
+        Map<String, Long> nativeMap = new HashMap<>();
+        
+        formatToNative = Collections.synchronizedMap(formatMap);
+        nativeToFormat = Collections.synchronizedMap(nativeMap);
+    }
+
+    private HaikuDataTransferer() {
+    }
+
+    public static synchronized HaikuDataTransferer getInstanceImpl() {
+        if (transferer == null)
+            transferer = new HaikuDataTransferer();
+        return transferer;
+    }
+
+    private Long addNative(String str) {
+        Long format = nativeToFormat.get(str);
+        if (format != null)
+            return format;
+        
+        format = Long.valueOf(index++);
+        formatToNative.put(format, str);
+        nativeToFormat.put(str, format);
+        return format;
+    }
+
+    @Override
+    public String getDefaultUnicodeEncoding() {
+        return "UTF-8";
+    }
+
+    @Override
+    public boolean isLocaleDependentTextFormat(long format) {
+        // todo: find out what this actually means
+        return format == FORMAT_STRING;
+    }
+
+    @Override
+    public boolean isFileFormat(long format) {
+        return format == FORMAT_URI_LIST;
+    }
+
+    @Override
+    public boolean isImageFormat(long format) {
+        return format == FORMAT_JPEG || format == FORMAT_PNG ||
+            format == FORMAT_TIFF;
+    }
+
+    @Override
+    protected boolean isURIListFormat(long format) {
+        return format == FORMAT_URI_LIST;
+    }
+
+    @Override
+    synchronized protected Long getFormatForNativeAsLong(String str) {
+        return addNative(str);
+    }
+
+    @Override
+    protected String getNativeForFormat(long format) {
+        return formatToNative.get(format);
+    }
+
+    @Override
+    public ToolkitThreadBlockedHandler getToolkitThreadBlockedHandler() {
+        if (handler == null)
+            handler = new HaikuToolkitThreadBlockedHandler();
+        return handler;
+    }
+
+    @Override
+    protected byte[] imageToPlatformBytes(Image image, long format)
+            throws IOException {
+        String mimeType = null;
+        if (format == FORMAT_PNG) {
+            mimeType = "image/png";
+        } else if (format == FORMAT_JPEG) {
+            mimeType = "image/jpeg";
+        } else {
+            // Check if an image MIME format.
+            try {
+                String nat = getNativeForFormat(format);
+                DataFlavor df = new DataFlavor(nat);
+                String primaryType = df.getPrimaryType();
+                if ("image".equals(primaryType)) {
+                    mimeType = df.getPrimaryType() + "/" + df.getSubType();
+                }
+            } catch (Exception e) {
+                // Not an image MIME format.
+            }
+        }
+        if (mimeType != null) {
+            return imageToStandardBytes(image, mimeType);
+        } else {
+            String nativeFormat = getNativeForFormat(format);
+            throw new IOException("Translation to " + nativeFormat +
+                                  " is not supported.");
+        }
+    }
+
+    @Override
+    protected String[] dragQueryFile(final byte[] bytes) {
+        // Confusing name, but this should translate a list of files given
+        // by the platform (i.e. Tracker) into an array of filename strings.
+        return null;
+    }
+
+    /**
+     * Translates either a byte array or an input stream which contain
+     * platform-specific image data in the given format into an Image.
+     */
+    @Override
+    protected Image platformImageBytesOrStreamToImage(InputStream stream,
+            byte[] bytes, long format) throws IOException {
+        String mimeType = null;
+        if (format == FORMAT_PNG) {
+            mimeType = "image/png";
+        } else if (format == FORMAT_JPEG) {
+            mimeType = "image/jpeg";
+        } else {
+            // Check if an image MIME format.
+            try {
+                String nat = getNativeForFormat(format);
+                DataFlavor df = new DataFlavor(nat);
+                String primaryType = df.getPrimaryType();
+                if ("image".equals(primaryType)) {
+                    mimeType = df.getPrimaryType() + "/" + df.getSubType();
+                }
+            } catch (Exception e) {
+                // Not an image MIME format.
+            }
+        }
+        if (mimeType != null) {
+            return standardImageBytesOrStreamToImage(stream, bytes, mimeType);
+        } else {
+            String nativeFormat = getNativeForFormat(format);
+            throw new IOException("Translation from " + nativeFormat +
+                                  " is not supported.");
+        }
+    }
+
+    @Override
+    protected ByteArrayOutputStream convertFileListToBytes(
+            ArrayList<String> fileList) throws IOException {
+        // This should do the opposite of dragQueryFile. Need to investigate
+        // what format Tracker provides files in for d&d
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        for (int i = 0; i < fileList.size(); i++) {
+            byte[] bytes = fileList.get(i).getBytes();
+            bos.write(bytes, 0, bytes.length);
+            bos.write(0);
+        }
+        return bos;
+    }
+}
+
+class HaikuToolkitThreadBlockedHandler implements ToolkitThreadBlockedHandler {
+
+    @Override
+    public void lock() {
+    }
+
+    @Override
+    public void unlock() {
+    }
+
+    @Override
+    public void enter() {
+    }
+
+    @Override
+    public void exit() {
+    }
+}
