92f401421c4f40c7ca8bebd784b77628ace9608c
hamish <none@none>
2012-07-06 17:40:59 +0100

    Add stub CUPSPrinter class from 3dEyes to get more apps to run

--- src/solaris/classes/sun/print/CUPSPrinter.java
+++ src/solaris/classes/sun/print/CUPSPrinter.java
@@ -75,14 +75,7 @@ public class CUPSPrinter  {
     private static int cupsPort = 0;
 
     static {
-        // load awt library to access native code
-        java.security.AccessController.doPrivileged(
-            new sun.security.action.LoadLibraryAction("awt"));
-        libFound = initIDs();
-        if (libFound) {
-           cupsServer = getCupsServer();
-           cupsPort = getCupsPort();
-        }
+        libFound = false;
     }
 
 
@@ -95,26 +88,6 @@ public class CUPSPrinter  {
         cupsMediaPrintables = null;
         cupsMediaTrays = null;
         initialized = false;
-
-        if (!libFound) {
-            throw new RuntimeException("cups lib not found");
-        } else {
-            // get page + tray names
-            media =  getMedia(printer);
-            if (media == null) {
-                // either PPD file is not found or printer is unknown
-                throw new RuntimeException("error getting PPD");
-            }
-
-            // get sizes
-            pageSizes = getPageSizes(printer);
-            if (pageSizes != null) {
-                nPageSizes = pageSizes.length/6;
-
-                nTrays = media.length/2-nPageSizes;
-                assert (nTrays >= 0);
-            }
-        }
     }
 
 
@@ -157,147 +130,13 @@ public class CUPSPrinter  {
      * Initialize media by translating PPD info to PrintService attributes.
      */
     private synchronized void initMedia() {
-        if (initialized) {
             return;
-        } else {
-            initialized = true;
-        }
-
-        if (pageSizes == null) {
-            return;
-        }
-
-        cupsMediaPrintables = new MediaPrintableArea[nPageSizes];
-        cupsMediaSNames = new MediaSizeName[nPageSizes];
-        cupsCustomMediaSNames = new CustomMediaSizeName[nPageSizes];
-
-        CustomMediaSizeName msn;
-        MediaPrintableArea mpa;
-        float length, width, x, y, w, h;
-
-        // initialize names and printables
-        for (int i=0; i<nPageSizes; i++) {
-            // media width and length
-            width = (float)(pageSizes[i*6]/PRINTER_DPI);
-            length = (float)(pageSizes[i*6+1]/PRINTER_DPI);
-            // media printable area
-            x = (float)(pageSizes[i*6+2]/PRINTER_DPI);
-            h = (float)(pageSizes[i*6+3]/PRINTER_DPI);
-            w = (float)(pageSizes[i*6+4]/PRINTER_DPI);
-            y = (float)(pageSizes[i*6+5]/PRINTER_DPI);
-
-            msn = new CustomMediaSizeName(media[i*2], media[i*2+1],
-                                          width, length);
-
-            // add to list of standard MediaSizeNames
-            if ((cupsMediaSNames[i] = msn.getStandardMedia()) == null) {
-                // add custom if no matching standard media
-                cupsMediaSNames[i] = msn;
-
-                // add this new custom msn to MediaSize array
-                if ((width > 0.0) && (length > 0.0)) {
-                    new MediaSize(width, length,
-                                  Size2DSyntax.INCH, msn);
-                }
-            }
-
-            // add to list of custom MediaSizeName
-            // for internal use of IPPPrintService
-            cupsCustomMediaSNames[i] = msn;
-
-            mpa = null;
-            try {
-                mpa = new MediaPrintableArea(x, y, w, h,
-                                             MediaPrintableArea.INCH);
-            } catch (IllegalArgumentException e) {
-                if (width > 0 && length > 0) {
-                    mpa = new MediaPrintableArea(0, 0, width, length,
-                                             MediaPrintableArea.INCH);
-                }
-            }
-            cupsMediaPrintables[i] = mpa;
-        }
-
-        // initialize trays
-        cupsMediaTrays = new MediaTray[nTrays];
-
-        MediaTray mt;
-        for (int i=0; i<nTrays; i++) {
-            mt = new CustomMediaTray(media[(nPageSizes+i)*2],
-                                     media[(nPageSizes+i)*2+1]);
-            cupsMediaTrays[i] = mt;
-        }
-
     }
 
     /**
      * Get CUPS default printer using IPP.
      */
     public static String getDefaultPrinter() {
-        try {
-            URL url = new URL("http", getServer(), getPort(), "");
-            final HttpURLConnection urlConnection =
-                IPPPrintService.getIPPConnection(url);
-
-            if (urlConnection != null) {
-                OutputStream os = (OutputStream)java.security.AccessController.
-                    doPrivileged(new java.security.PrivilegedAction() {
-                        public Object run() {
-                            try {
-                                return urlConnection.getOutputStream();
-                            } catch (Exception e) {
-                            }
-                            return null;
-                        }
-                    });
-
-                if (os == null) {
-                    return null;
-                }
-
-                AttributeClass attCl[] = {
-                    AttributeClass.ATTRIBUTES_CHARSET,
-                    AttributeClass.ATTRIBUTES_NATURAL_LANGUAGE,
-                    new AttributeClass("requested-attributes",
-                                       AttributeClass.TAG_KEYWORD,
-                                       "printer-name")
-                };
-
-                if (IPPPrintService.writeIPPRequest(os,
-                                        IPPPrintService.OP_CUPS_GET_DEFAULT,
-                                        attCl)) {
-
-                    HashMap defaultMap = null;
-                    InputStream is = urlConnection.getInputStream();
-                    HashMap[] responseMap = IPPPrintService.readIPPResponse(
-                                         is);
-                    is.close();
-
-                    if (responseMap.length > 0) {
-                        defaultMap = responseMap[0];
-                    }
-
-                    if (defaultMap == null) {
-                        os.close();
-                        urlConnection.disconnect();
-                        return null;
-                    }
-
-                    AttributeClass attribClass = (AttributeClass)
-                        defaultMap.get("printer-name");
-
-                    if (attribClass != null) {
-                        String nameStr = attribClass.getStringValue();
-                        os.close();
-                        urlConnection.disconnect();
-                        return nameStr;
-                    }
-                }
-                os.close();
-                urlConnection.disconnect();
-            }
-        } catch (Exception e) {
-        }
         return null;
     }
 
@@ -306,72 +145,7 @@ public class CUPSPrinter  {
      * Get list of all CUPS printers using IPP.
      */
     public static String[] getAllPrinters() {
-        try {
-            URL url = new URL("http", getServer(), getPort(), "");
-
-            final HttpURLConnection urlConnection =
-                IPPPrintService.getIPPConnection(url);
-
-            if (urlConnection != null) {
-                OutputStream os = (OutputStream)java.security.AccessController.
-                    doPrivileged(new java.security.PrivilegedAction() {
-                        public Object run() {
-                            try {
-                                return urlConnection.getOutputStream();
-                            } catch (Exception e) {
-                            }
-                            return null;
-                        }
-                    });
-
-                if (os == null) {
-                    return null;
-                }
-
-                AttributeClass attCl[] = {
-                    AttributeClass.ATTRIBUTES_CHARSET,
-                    AttributeClass.ATTRIBUTES_NATURAL_LANGUAGE,
-                    new AttributeClass("requested-attributes",
-                                       AttributeClass.TAG_KEYWORD,
-                                       "printer-uri-supported")
-                };
-
-                if (IPPPrintService.writeIPPRequest(os,
-                                IPPPrintService.OP_CUPS_GET_PRINTERS, attCl)) {
-
-                    InputStream is = urlConnection.getInputStream();
-                    HashMap[] responseMap =
-                        IPPPrintService.readIPPResponse(is);
-
-                    is.close();
-                    os.close();
-                    urlConnection.disconnect();
-
-                    if (responseMap == null || responseMap.length == 0) {
-                        return null;
-                    }
-
-                    ArrayList printerNames = new ArrayList();
-                    for (int i=0; i< responseMap.length; i++) {
-                        AttributeClass attribClass = (AttributeClass)
-                            responseMap[i].get("printer-uri-supported");
-
-                        if (attribClass != null) {
-                            String nameStr = attribClass.getStringValue();
-                            printerNames.add(nameStr);
-                        }
-                    }
-                    return (String[])printerNames.toArray(new String[] {});
-                } else {
-                    os.close();
-                    urlConnection.disconnect();
-                }
-            }
-
-        } catch (Exception e) {
-        }
         return null;
-
     }
 
     /**
@@ -392,14 +166,7 @@ public class CUPSPrinter  {
      * Detects if CUPS is running.
      */
     public static boolean isCupsRunning() {
-        IPPPrintService.debug_println(debugPrefix+"libFound "+libFound);
-        if (libFound) {
-            IPPPrintService.debug_println(debugPrefix+"CUPS server "+getServer()+
-                                          " port "+getPort());
-            return canConnect(getServer(), getPort());
-        } else {
             return false;
-        }
     }
 
 
