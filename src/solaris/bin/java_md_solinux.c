f062696188c2502d50fb5503cbae483cd4e206ba
spoole <spoole@linux.vnet.ibm.com>
2012-07-12 13:50:22 +0100

    Updated using pthreads in java_md_solinux.c to just rely on USE_PTHREADS. Removed the superflous __linux__ as  USE_PTHREADS is already explictly turned on for linux builds

--- src/solaris/bin/java_md_solinux.c
+++ src/solaris/bin/java_md_solinux.c
@@ -981,7 +981,7 @@ void SplashFreeLibrary() {
 int
 ContinueInNewThread0(int (JNICALL *continuation)(void *), jlong stack_size, void * args) {
     int rslt;
-#ifdefined(__linux__) || defined(USE_PTHREADS)
+#ifdef USE_PTHREADS
     pthread_t tid;
     pthread_attr_t attr;
     pthread_attr_init(&attr);
