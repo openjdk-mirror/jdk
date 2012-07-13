6b666a22aecb764dfa51d119aaad36e67ff42e49
hamish <none@none>
2012-07-13 20:31:20 +0100

    Nick the ring buffer implementation from the Mac port

--- src/solaris/native/com/sun/media/sound/PLATFORM_API_HaikuOS_Utils.h
+++ src/solaris/native/com/sun/media/sound/PLATFORM_API_HaikuOS_Utils.h
@@ -46,4 +46,219 @@ private:
 	BLocker							lock;
 };
 
+class RingBuffer {
+public:
+    RingBuffer() : pBuffer(NULL), nBufferSize(0) {
+        pthread_mutex_init(&lockMutex, NULL);
+    }
+    ~RingBuffer() {
+        Deallocate();
+        pthread_mutex_destroy(&lockMutex);
+    }
+
+    // extraBytes: number of additionally allocated bytes to prevent data
+    // overlapping when almost whole buffer is filled
+    // (required only if Write() can override the buffer)
+    bool Allocate(int requestedBufferSize, int extraBytes) {
+        int fullBufferSize = requestedBufferSize + extraBytes;
+        int powerOfTwo = 1;
+        while (powerOfTwo < fullBufferSize) {
+            powerOfTwo <<= 1;
+        }
+        pBuffer = (Byte*)malloc(powerOfTwo);
+        if (pBuffer == NULL) {
+            ERROR0("RingBuffer::Allocate: OUT OF MEMORY\n");
+            return false;
+        }
+
+        nBufferSize = requestedBufferSize;
+        nAllocatedBytes = powerOfTwo;
+        nPosMask = powerOfTwo - 1;
+        nWritePos = 0;
+        nReadPos = 0;
+        nFlushPos = -1;
+
+        TRACE2("RingBuffer::Allocate: OK, bufferSize=%d, allocated:%d\n", nBufferSize, nAllocatedBytes);
+        return true;
+    }
+
+    void Deallocate() {
+        if (pBuffer) {
+            free(pBuffer);
+            pBuffer = NULL;
+            nBufferSize = 0;
+        }
+    }
+
+    inline int GetBufferSize() {
+        return nBufferSize;
+    }
+
+    inline int GetAllocatedSize() {
+        return nAllocatedBytes;
+    }
+
+    // gets number of bytes available for reading
+    int GetValidByteCount() {
+        lock();
+        INT64 result = nWritePos - (nFlushPos >= 0 ? nFlushPos : nReadPos);
+        unlock();
+        return result > (INT64)nBufferSize ? nBufferSize : (int)result;
+    }
+
+    int Write(void *srcBuffer, int len, bool preventOverflow) {
+        lock();
+        TRACE2("RingBuffer::Write (%d bytes, preventOverflow=%d)\n", len, preventOverflow ? 1 : 0);
+        TRACE2("  writePos = %lld (%d)", (long long)nWritePos, Pos2Offset(nWritePos));
+        TRACE2("  readPos=%lld (%d)", (long long)nReadPos, Pos2Offset(nReadPos));
+        TRACE2("  flushPos=%lld (%d)\n", (long long)nFlushPos, Pos2Offset(nFlushPos));
+
+        INT64 writePos = nWritePos;
+        if (preventOverflow) {
+            INT64 avail_read = writePos - (nFlushPos >= 0 ? nFlushPos : nReadPos);
+            if (avail_read >= (INT64)nBufferSize) {
+                // no space
+                TRACE0("  preventOverlow: OVERFLOW => len = 0;\n");
+                len = 0;
+            } else {
+                int avail_write = nBufferSize - (int)avail_read;
+                if (len > avail_write) {
+                    TRACE2("  preventOverlow: desrease len: %d => %d\n", len, avail_write);
+                    len = avail_write;
+                }
+            }
+        }
+        unlock();
+
+        if (len > 0) {
+
+            write((Byte *)srcBuffer, Pos2Offset(writePos), len);
+
+            lock();
+            TRACE4("--RingBuffer::Write writePos: %lld (%d) => %lld, (%d)\n",
+                (long long)nWritePos, Pos2Offset(nWritePos), (long long)nWritePos + len, Pos2Offset(nWritePos + len));
+            nWritePos += len;
+            unlock();
+        }
+        return len;
+    }
+
+    int Read(void *dstBuffer, int len) {
+        lock();
+        TRACE1("RingBuffer::Read (%d bytes)\n", len);
+        TRACE2("  writePos = %lld (%d)", (long long)nWritePos, Pos2Offset(nWritePos));
+        TRACE2("  readPos=%lld (%d)", (long long)nReadPos, Pos2Offset(nReadPos));
+        TRACE2("  flushPos=%lld (%d)\n", (long long)nFlushPos, Pos2Offset(nFlushPos));
+
+        applyFlush();
+        INT64 avail_read = nWritePos - nReadPos;
+        // check for overflow
+        if (avail_read > (INT64)nBufferSize) {
+            nReadPos = nWritePos - nBufferSize;
+            avail_read = nBufferSize;
+            TRACE0("  OVERFLOW\n");
+        }
+        INT64 readPos = nReadPos;
+        unlock();
+
+        if (len > (int)avail_read) {
+            TRACE2("  RingBuffer::Read - don't have enough data, len: %d => %d\n", len, (int)avail_read);
+            len = (int)avail_read;
+        }
+
+        if (len > 0) {
+
+            read((Byte *)dstBuffer, Pos2Offset(readPos), len);
+
+            lock();
+            if (applyFlush()) {
+                // just got flush(), results became obsolete
+                TRACE0("--RingBuffer::Read, got Flush, return 0\n");
+                len = 0;
+            } else {
+                TRACE4("--RingBuffer::Read readPos: %lld (%d) => %lld (%d)\n",
+                    (long long)nReadPos, Pos2Offset(nReadPos), (long long)nReadPos + len, Pos2Offset(nReadPos + len));
+                nReadPos += len;
+            }
+            unlock();
+        } else {
+            // underrun!
+        }
+        return len;
+    }
+
+    // returns number of the flushed bytes
+    int Flush() {
+        lock();
+        INT64 flushedBytes = nWritePos - (nFlushPos >= 0 ? nFlushPos : nReadPos);
+        nFlushPos = nWritePos;
+        unlock();
+        return flushedBytes > (INT64)nBufferSize ? nBufferSize : (int)flushedBytes;
+    }
+
+private:
+    Byte *pBuffer;
+    int nBufferSize;
+    int nAllocatedBytes;
+    INT64 nPosMask;
+
+    pthread_mutex_t lockMutex;
+
+    volatile INT64 nWritePos;
+    volatile INT64 nReadPos;
+    // Flush() sets nFlushPos value to nWritePos;
+    // next Read() sets nReadPos to nFlushPos and resests nFlushPos to -1
+    volatile INT64 nFlushPos;
+
+    inline void lock() {
+        pthread_mutex_lock(&lockMutex);
+    }
+    inline void unlock() {
+        pthread_mutex_unlock(&lockMutex);
+    }
+
+    inline bool applyFlush() {
+        if (nFlushPos >= 0) {
+            nReadPos = nFlushPos;
+            nFlushPos = -1;
+            return true;
+        }
+        return false;
+    }
+
+    inline int Pos2Offset(INT64 pos) {
+        return (int)(pos & nPosMask);
+    }
+
+    void write(Byte *srcBuffer, int dstOffset, int len) {
+        int dstEndOffset = dstOffset + len;
+
+        int lenAfterWrap = dstEndOffset - nAllocatedBytes;
+        if (lenAfterWrap > 0) {
+            // dest.buffer does wrap
+            len = nAllocatedBytes - dstOffset;
+            memcpy(pBuffer+dstOffset, srcBuffer, len);
+            memcpy(pBuffer, srcBuffer+len, lenAfterWrap);
+        } else {
+            // dest.buffer does not wrap
+            memcpy(pBuffer+dstOffset, srcBuffer, len);
+        }
+    }
+
+    void read(Byte *dstBuffer, int srcOffset, int len) {
+        int srcEndOffset = srcOffset + len;
+
+        int lenAfterWrap = srcEndOffset - nAllocatedBytes;
+        if (lenAfterWrap > 0) {
+            // need to unwrap data
+            len = nAllocatedBytes - srcOffset;
+            memcpy(dstBuffer, pBuffer+srcOffset, len);
+            memcpy(dstBuffer+len, pBuffer, lenAfterWrap);
+        } else {
+            // source buffer is not wrapped
+            memcpy(dstBuffer, pBuffer+srcOffset, len);
+        }
+    }
+};
+
 #endif // PLATFORM_API_HAIKUOS_UTILS_H_INCLUDED
