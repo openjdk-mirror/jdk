0a6ab07bedb8c60227ba819d07f3e9ab31a0e66f
hamish <none@none>
2012-07-29 21:10:41 +0100

    Complete MIDI out support -- untested

--- src/solaris/native/com/sun/media/sound/PLATFORM_API_HaikuOS_MidiOut.cpp
+++ src/solaris/native/com/sun/media/sound/PLATFORM_API_HaikuOS_MidiOut.cpp
@@ -28,6 +28,11 @@
 
 #if USE_PLATFORM_MIDI_OUT == TRUE
 
+#include <MidiConsumer.h>
+#include <MidiProducer.h>
+
+#include <string.h>
+
 extern "C" {
 #include "PlatformMidi.h"
 }
@@ -35,22 +40,22 @@ extern "C" {
 #include "PLATFORM_API_HaikuOS_Utils.h"
 
 
-extern MidiDeviceCache midiCache;
+MidiDeviceCache midiCache;
 
 
 char* MIDI_OUT_GetErrorStr(INT32 err) {
-    // TODO
+    return strerror(err);
 }
 
 
 INT32 MIDI_OUT_GetNumDevices() {
-    return cache.ConsumerCount();
+    return midiCache.ConsumerCount();
 }
 
 
 INT32 MIDI_OUT_GetDeviceName(INT32 deviceIndex, char *name, UINT32 nameLength) {
     BMidiConsumer* consumer;
-    if (cache.GetConsumer(deviceIndex, &consumer) == B_OK) {
+    if (midiCache.GetConsumer(deviceIndex, &consumer) == B_OK) {
         strlcpy(name, consumer->Name(), nameLength);
         return MIDI_SUCCESS;
     }
@@ -61,7 +66,7 @@ INT32 MIDI_OUT_GetDeviceName(INT32 deviceIndex, char *name, UINT32 nameLength) {
 
 INT32 MIDI_OUT_GetDeviceVendor(INT32 deviceIndex, char *name, UINT32 nameLength) {
     BMidiConsumer* consumer;
-    if (cache.GetConsumer(deviceIndex, &consumer) == B_OK) {
+    if (midiCache.GetConsumer(deviceIndex, &consumer) == B_OK) {
         strlcpy(name, "", nameLength);
         return MIDI_SUCCESS;
     }
@@ -72,7 +77,7 @@ INT32 MIDI_OUT_GetDeviceVendor(INT32 deviceIndex, char *name, UINT32 nameLength)
 
 INT32 MIDI_OUT_GetDeviceDescription(INT32 deviceIndex, char *name, UINT32 nameLength) {
     BMidiConsumer* consumer;
-    if (cache.GetConsumer(deviceIndex, &consumer) == B_OK) {
+    if (midiCache.GetConsumer(deviceIndex, &consumer) == B_OK) {
         strlcpy(name, "", nameLength);
         return MIDI_SUCCESS;
     }
@@ -83,7 +88,7 @@ INT32 MIDI_OUT_GetDeviceDescription(INT32 deviceIndex, char *name, UINT32 nameLe
 
 INT32 MIDI_OUT_GetDeviceVersion(INT32 deviceIndex, char *name, UINT32 nameLength) {
     BMidiConsumer* consumer;
-    if (cache.GetConsumer(deviceIndex, &consumer) == B_OK) {
+    if (midiCache.GetConsumer(deviceIndex, &consumer) == B_OK) {
         strlcpy(name, "", nameLength);
         return MIDI_SUCCESS;
     }
@@ -92,25 +97,78 @@ INT32 MIDI_OUT_GetDeviceVersion(INT32 deviceIndex, char *name, UINT32 nameLength
 }
 
 
+struct MidiOutHandle {
+    BMidiLocalProducer* localProducer;
+    BMidiConsumer* remoteConsumer;
+};
+
+
 INT32 MIDI_OUT_OpenDevice(INT32 deviceIndex, MidiDeviceHandle** handle) {
+    BMidiConsumer* consumer;
+    if (midiCache.GetConsumer(deviceIndex, &consumer) != B_OK) {
+        return MIDI_INVALID_DEVICEID;
+    }
+
+    BMidiLocalProducer* producer = new BMidiLocalProducer();
+    if (!producer->IsValid()) {
+        producer->Release();
+        return MIDI_INVALID_DEVICEID;
+    }
+
+    status_t result = producer->Connect(consumer);
+    if (result != B_OK) {
+        producer->Release();
+        return result;
+    }
+
+    MidiOutHandle* outHandle = new MidiOutHandle();
+    outHandle->localProducer = producer;
+    outHandle->remoteConsumer = consumer;
+
+    *handle = new MidiDeviceHandle();
+    (*handle)->deviceHandle = (void*)outHandle;
+    (*handle)->startTime = system_time_nsecs();
+    return MIDI_SUCCESS;
 }
 
 
 INT32 MIDI_OUT_CloseDevice(MidiDeviceHandle* handle) {
+    MidiOutHandle* outHandle = (MidiOutHandle*)handle->deviceHandle;
+
+    outHandle->localProducer->Disconnect(outHandle->remoteConsumer);
+
+    outHandle->localProducer->Release();
+    delete outHandle;
+    delete handle;
+
+    return MIDI_SUCCESS;
 }
 
 
 INT64 MIDI_OUT_GetTimeStamp(MidiDeviceHandle* handle) {
+    return system_time_nsecs() - handle->startTime;
 }
 
 
 INT32 MIDI_OUT_SendShortMessage(MidiDeviceHandle* handle, UINT32 packedMsg,
                                 UINT32 timestamp) {
+    MidiOutHandle* outHandle = (MidiOutHandle*)handle->deviceHandle;
+
+    outHandle->localProducer->SprayData((void*)&packedMsg, sizeof(UINT32),
+        true, (bigtime_t)timestamp);
+
+    return MIDI_SUCCESS;
 }
 
 
 INT32 MIDI_OUT_SendLongMessage(MidiDeviceHandle* handle, UBYTE* data,
                                UINT32 size, UINT32 timestamp) {
+    MidiOutHandle* outHandle = (MidiOutHandle*)handle->deviceHandle;
+
+    outHandle->localProducer->SprayData((void*)data, size, true,
+        (bigtime_t)timestamp);
+
+    return MIDI_SUCCESS;
 }
 
 
