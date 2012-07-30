2a054968e4e6ef17e726c69a460a0f008168eefd
hamish <none@none>
2012-07-30 22:36:57 +0100

    Correct handling of short message output

--- src/solaris/native/com/sun/media/sound/PLATFORM_API_HaikuOS_MidiOut.cpp
+++ src/solaris/native/com/sun/media/sound/PLATFORM_API_HaikuOS_MidiOut.cpp
@@ -43,6 +43,28 @@ extern "C" {
 MidiDeviceCache midiCache;
 
 
+static int CHANNEL_MESSAGE_LENGTH[] = {
+    -1, -1, -1, -1, -1, -1, -1, -1, 3, 3, 3, 3, 2, 2, 3 };
+/*                                 8x 9x Ax Bx Cx Dx Ex */
+
+static int SYSTEM_MESSAGE_LENGTH[] = {
+    -1, 2, 3, 2, -1, -1, 1, 1, 1, -1, 1, 1, 1, -1, 1, 1 };
+/*  F0 F1 F2 F3  F4  F5 F6 F7 F8  F9 FA FB FC  FD FE FF */
+
+
+// the returned length includes the status byte.
+// for illegal messages, -1 is returned.
+static int getShortMessageLength(int status) {
+        int     dataLength = 0;
+        if (status < 0xF0) { // channel voice message
+                dataLength = CHANNEL_MESSAGE_LENGTH[(status >> 4) & 0xF];
+        } else {
+                dataLength = SYSTEM_MESSAGE_LENGTH[status & 0xF];
+        }
+        return dataLength;
+}
+
+
 char* MIDI_OUT_GetErrorStr(INT32 err) {
     return strerror(err);
 }
@@ -136,8 +158,15 @@ INT32 MIDI_OUT_SendShortMessage(MidiDeviceHandle* handle, UINT32 packedMsg,
                                 UINT32 timestamp) {
     MidiOutHandle* outHandle = (MidiOutHandle*)handle->deviceHandle;
 
-    outHandle->localProducer->SprayData((void*)&packedMsg, sizeof(UINT32),
-        true, (bigtime_t)timestamp);
+    UBYTE message[3];
+    message[0] = (UBYTE)(packedMsg & 0xff); // status
+    message[1] = (UBYTE)((packedMsg >> 8) & 0xff); // possible data1
+    message[2] = (UBYTE)((packedMsg >> 16) & 0xff); // possible data2
+
+    size_t length = getShortMessageLength((int)message[0]);
+
+    outHandle->localProducer->SprayData((void*)message, length, true,
+        (bigtime_t)timestamp);
 
     return MIDI_SUCCESS;
 }
